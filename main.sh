84465cf470a4ca1749b65e501014b3743019eb04
Gabriel Crasmaruc <gabriel.crasmaruc@protonmail.com>
2026-01-18 19:58:34 +0200

    Implement infinite loop for calculator input and logging

--- main.sh
+++ main.sh
@@ -2,12 +2,22 @@
 
 operation_regex="^-?[0-9]+(.[0-9]+)? [+*/^-] -?[0-9]+(.[0-9]+)?$"
 
-echo "Enter an arithmetic operation:"
-read -r operation
+echo "Welcome to the basic calculator!" | tee operation_history.txt
 
-if [[ "$operation" =~ $operation_regex ]]; then
-  arithmetic_result=$(echo "scale=2; $operation" | bc -l)
-  printf "%s\n" "$arithmetic_result"
-else
-  echo "Operation check failed!"
-fi
+while true; do
+  echo "Enter an arithmetic operation or type 'quit' to quit:" | tee -a operation_history.txt
+  read -r operation
+  echo "$operation" >> operation_history.txt
+
+  if [[ "$operation" == "quit" ]]; then
+    echo "Goodbye!" | tee -a operation_history.txt
+    break
+  fi
+
+  if [[ "$operation" =~ $operation_regex ]]; then
+    arithmetic_result=$(echo "scale=2; $operation" | bc -l)
+    echo "$arithmetic_result" | tee -a operation_history.txt
+  else
+    echo "Operation check failed!" | tee -a operation_history.txt
+  fi
+done
