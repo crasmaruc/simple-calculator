96bf120aff10a9f34cb56cc2ad7df14812fe8a4a
Gabriel Crasmaruc <gabriel.crasmaruc@protonmail.com>
2026-01-18 19:19:50 +0200

    Add exponential operator and use bc utility

--- main.sh
+++ main.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-operation_regex="^-?[0-9]+ [+*/-] -?[0-9]+$"
+operation_regex="^-?[0-9]+(.[0-9]+)? [+*/^-] -?[0-9]+(.[0-9]+)?$"
 
 echo "Enter an arithmetic operation:"
 read -r operation
 
 if [[ "$operation" =~ $operation_regex ]]; then
-  arithmetic_result=$(( operation ))
+  arithmetic_result=$(echo "scale=2; $operation" | bc -l)
   printf "%s\n" "$arithmetic_result"
 else
   echo "Operation check failed!"
